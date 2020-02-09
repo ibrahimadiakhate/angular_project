936b82b2065faefbb907dbe78223a997bb800ea0
Ibrahima <ide@magellium>
2020-02-09 16:19:09 +0100

    Ajout nouveau module via avec la commande ng g c ervers

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core'
-@Component({
-    selector:'app-server',
-    templateUrl:'app/ServerComponent/'
-})
-export ServerComponent{
+import { Component } from '@angular/core';
 
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: []
+})
+export class ServerComponent {
 }
